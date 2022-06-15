407c985aee7ec0661ed23816623eecd240e32808
rachid <azeroual.rachid1@gmail.com>
2022-06-15 12:44:35 -0400

    first commit

---
+++ src/main/java/com/rachid/assessmentjpa/AssessmentJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rachid.assessmentjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rachid/assessmentjpa/AssessmentJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachid.assessmentjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
